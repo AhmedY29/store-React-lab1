84869a0d9df4938f8c806d23eb0b547f004e8d8c
AhmedY29 <ahmedys2001@gmail.com>
2025-05-20 13:00:49 +0300

    complete all sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Card from "./components/Card";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Card />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="flex flex-col items-center justify-center w-full mt-10">
+      <div className="footer-content flex justify-between items-center w-[80%]  bg-gray-100 rounded-2xl p-4">
+        <div className="footer-text">
+          <h1>Bose Bluetooth Wireless Heeadphones</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas,
+            perspiciatis provident.
+          </p>
+        </div>
+        <div className="footer-price flex flex-col gap-2">
+          <h1 className="line-through text-sm font-light">$260</h1>
+          <h1 className="text-green-400">$219</h1>
+          <button className="bg-amber-300 w-25 h-10">Shop Now</button>
+        </div>
+
+        <div className="footer-img">
+          <img
+            src="https://m.media-amazon.com/images/I/71y9B19C5LL._AC_UL960_FMwebp_QL65_.jpg"
+            width={100}
+            alt=""
+          />
+        </div>
+      </div>
+      <h1>
+        Made By{" "}
+        <a
+          className="text-blue-300"
+          href="https://github.com/AhmedY29"
+          target="_blank"
+        >
+          Ahmed Alsaleh
+        </a>
+      </h1>
+    </footer>
+  );
+}
+
+export default Footer;
